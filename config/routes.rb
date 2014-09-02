46b46a887c5295004eb590ea12a5814c494e85d9
fitzhayashida <fitzhayashida@gmail.com>
2014-09-02 23:37:30 +0000

    login functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'users/new'
+
+  get 'users/create'
+
   get 'movies/index'
 
   get 'movies/show'
@@ -9,7 +17,17 @@ Rails.application.routes.draw do
   get 'movies/edit'
 
   RottenMangoes::Application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'users/new'
+
+  get 'users/create'
+
     resources :movies
+    resources :users, only: [:new, :create]
+    resources :sessions, only: [:new, :create]
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
